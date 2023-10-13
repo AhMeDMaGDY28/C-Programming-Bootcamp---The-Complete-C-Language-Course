fb1f6f3654025557c5f00ce45d98b37f5c290318
Ahmed <ray2legand128@gmail.com>
2023-10-13 23:19:58 +0300

    1

---
+++ 06-Welcome/Challenge #1 - Print Your First Name And Last Name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print my first name and my last name
+ * return always 0
+ */
+int main()
+{
+	printf("First_Name: Ahmed\n last_Name: Magdy \n");
+	return (0);
+}
