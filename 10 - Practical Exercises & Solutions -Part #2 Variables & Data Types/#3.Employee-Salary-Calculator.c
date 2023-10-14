79f02036e73fa6e0db7b936bc33983fac11d97b8
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-14 23:17:47 +0300

    1

--- 10 - Practical Exercises & Solutions -Part #2 Variables & Data Types/#3.Employee-Salary-Calculator.c
+++ 10 - Practical Exercises & Solutions -Part #2 Variables & Data Types/#3.Employee-Salary-Calculator.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function calculates
+ * and prints the monthly salary based on the user-provided values for
+ * the salary per hour and total hours worked.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+
 int main(void)
 {
-    int salary_hour, total_hours, monthly_salary;
+	int salary_hour, total_hours, monthly_salary;
 
-    printf("enter the salary per hour");
-    scanf("%d",salary_hour);
+	printf("enter the salary per hour");
+	scanf("%d", &salary_hour);
 
-    printf("total hours worked");
-    scanf("%d",total_hours);
+	printf("total hours worked");
+	scanf("%d", &total_hours);
 
-    monthly_salary = salary_hour * total_hours;
-    
-       printf("your salary in this month is %d", monthly_salary);
+	monthly_salary = salary_hour * total_hours;
 
+	printf("your salary in this month is %d", monthly_salary);
+	return (0);
 }
