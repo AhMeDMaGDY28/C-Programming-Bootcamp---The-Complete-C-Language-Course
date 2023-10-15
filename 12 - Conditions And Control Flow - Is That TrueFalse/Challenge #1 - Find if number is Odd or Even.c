17fe512ac38de240676e27761c1d4736f3afe3d9
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-15 21:32:08 +0300

    Challenge #1 - Find if number is Odd or Even

---
+++ 12 - Conditions And Control Flow - Is That TrueFalse/Challenge #1 - Find if number is Odd or Even.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function checks if a number is even or odd and prints the result.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+
+int main(void)
+{
+	int X;
+
+	printf("enter your number : \n");
+	scanf("%d", &X);
+	if (X % 2 == 0)
+	{
+		printf("%d is an even", X);
+	}
+	else
+	{
+		printf("%d is an even", X);
+	}
+
+	return (0);
+}
