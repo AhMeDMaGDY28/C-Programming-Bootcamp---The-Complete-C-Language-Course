d5d4872f0545196fd2989c0a41fb15985846f30e
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-16 15:43:34 +0300

    #2.Finding_Maximum_Index_in_Array

---
+++ 19 - Arrays - Concept & 1D Arrays/#2.Finding_Maximum_Index_in_Array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function checks for the maximum number in the array.
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int i, max_digit, index;
+	int number[5];
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("index %d = Enter value :\n", i);
+		scanf("%d", &number[i]);
+
+		max_digit = 0;
+		if (number[i] > max_digit)
+		{
+			max_digit = number[i];
+			index = i;
+		}
+	}
+	printf("the  max digit index is %d\n", index);
+	printf("the max digit is %d\n", max_digit);
+	printf("\n");
+	return (0);
+}
