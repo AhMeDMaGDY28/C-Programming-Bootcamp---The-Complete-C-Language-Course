3a78613c7cc24be0ee8cc8d86662c77c3653394c
AhMeDMaGDY28 <ray2legand@gmail.com>
2023-10-21 20:33:00 +0300

    #1 - Print Greetings!

---
+++ 16 - Practical Exercises & Solutions - Part #4 Loops in C/#9.Practice_Exercise_#1_Subtracting_EvenDigitsSum_and_OddDigitsSum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program calculates the sum of integers
+ * from 15 to a user-defined number 'n.'
+ * It prompts the user to enter a value for
+ * 'n' and then computes the sum of all integers
+ * from 15 to 'n' that are divisible by 15. The program displays the final sum.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+	int num;
+	int evensum = 0, oddsum = 0;
+
+	printf("enter num : ");
+	scanf("%d", &num);
+
+	return (0);
+}
---
+++ 17 - Functions - Theory & Introduction!/#1 - Print Greetings!.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * printGreetings - Greeting Function
+ *
+ * Description: This function is responsible for printing a series of greeting messages
+ * to the standard output. It displays messages to welcome the user and provide some words
+ * of encouragement.
+ *
+ * Return: This function does not return any value (void).
+ *
+ * Author: Ahmed Magdy
+ * School: ALX CO1 BLENDED
+ */
+void printGreetings(void)
+{
+	printf("Greeting Function\n");
+	printf("Hello!\n");
+	printf("Welcome to the best course (ever) in C programming language\n");
+	printf("Good Luck\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program serves as the entry point for the application. It calls the
+ * 'printGreetings' function to display a series of greeting messages. After calling the
+ * function, the program adds a newline character and then returns 0 to indicate successful
+ * execution.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+	printGreetings(); /* Call the greeting function*/
+	printf("\n");	/*Display a newline character*/
+
+	return 0; /*Return 0 to indicate successful execution*/
+}
